29f0d6d9b74016f55931ee0cdcee41e91cc19c1c
y0ngma <takeit2sy@pusan.ac.kr>
2023-06-23 10:39:43 +0900

    FIX: Add shell script and Succeed install docker

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Use following command to set up the repository
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+# 최신버전 설치하기
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# docker 명령어 sudo로 실행전 권한부여필수
+sudo chown vagrant:vagrant /var/run/docker.sock
+
+# sudo 없이 작동 테스트 (터미널 재시작 하지 않고 되어야 함)
+docker run hello-world
